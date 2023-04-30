c2752ae2cc03d37023004e5c38f322ca1c1599df
Charles Hudson <charles@bunch.capital>
2023-04-30 22:36:30 +0200

    Fixing eslint fix on save again

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -195,6 +195,16 @@ require('lspconfig').lua_ls.setup({
   },
 })
 
+require('lspconfig').eslint.setup({
+  on_attach = function(client, bufnr)
+    on_attach(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end
+})
+
 require('lspconfig').stylelint_lsp.setup({
   on_attach = on_attach,
   filetypes = { 'html', 'css', 'less', 'scss', 'svelte' },
