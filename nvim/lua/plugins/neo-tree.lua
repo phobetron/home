4864c8f55e3d210b1ec402639bfff3814e24f2e7
Charles Hudson <charles.hudson@contentful.com>
2025-09-21 15:32:07 +0200

    Adding extras

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,35 @@
+local function set_colors()
+  local visual = vim.api.nvim_get_hl(0, { name = "Visual" })
+  vim.api.nvim_set_hl(0, "WindowPickerStatusLine", { fg = visual.fg, bg = visual.bg })
+  vim.api.nvim_set_hl(0, "WindowPickerStatusLineNC", { fg = visual.fg, bg = visual.bg })
+  vim.api.nvim_set_hl(0, "WindowPickerWinBar", { fg = visual.fg, bg = visual.bg })
+  vim.api.nvim_set_hl(0, "WindowPickerWinBarNC", { fg = visual.fg, bg = visual.bg })
+end
+
+set_colors()
+
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = function(_, opts)
+      opts.window.mappings["<2-LeftMouse>"] = "open_with_window_picker"
+      opts.window.mappings["<cr>"] = "open_with_window_picker"
+      opts.window.mappings["S"] = "split_with_window_picker"
+      opts.window.mappings["s"] = "vsplit_with_window_picker"
+    end,
+  },
+
+  {
+    "s1n7ax/nvim-window-picker",
+    opts = {
+      show_prompt = false,
+      selection_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      picker_config = {
+        handle_mouse_click = true,
+        statusline_winbar_picker = {
+          use_winbar = "smart",
+        },
+      },
+    },
+  },
+}
---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    views = {
+      cmdline_popup = {
+        position = {
+          row = "50%",
+          col = "50%",
+        },
+      },
+    },
+  },
+}
