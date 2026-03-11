52cc795c957b8064d083d00947f623521f5b620b
Charles Hudson <charles.hudson@contentful.com>
2026-03-11 13:25:36 +0100

    Configuring snacks picker

---
+++ nvim/lua/plugins/picker.lua
@@ -0,0 +1,13 @@
+return {
+  "snacks.nvim",
+  opts = {
+    picker = {
+      formatters = {
+        file = {
+          min_width = 75,
+          truncate = "left",
+        },
+      },
+    },
+  },
+}
